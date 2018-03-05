17a1ee2dfdc47b298602db2ab5e1204c4f176b93
zednis <lokipatera@gmail.com>
2018-03-04 17:59:52 -0700

    fixed typo

--- aws_credential_travis.sh
+++ aws_credential_travis.sh
@@ -8,7 +8,7 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
-cat ~/.aws/config << EOL
+cat > ~/.aws/config << EOL
 [default]
 region = us-east-1
 output = json
